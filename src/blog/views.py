d667a977d36142ec26c9da56861d1a0adcc84f51
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-07 20:15:56 +0100

    like button updated

--- src/blog/views.py
+++ src/blog/views.py
@@ -84,3 +84,4 @@ def like(request, slug):
             like_qs[0].delete()
         else:
             Like.objects.create(user=request.user, post=obj)
+        return redirect('blog:detail', slug=slug)
